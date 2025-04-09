a202ddff7f8d56b126cc4a4ca4634aa0feabd10e
Samuel Nunes <samuelluiznunes10@gmail.com>
2025-04-08 22:40:46 -0300

    extracao token

---
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -0,0 +1,64 @@
+package plataformaFilmes;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlataformaFilmesTest {
+
+    public static String token;
+
+    @BeforeAll
+    public static void validarLoginMap(){
+        RestAssured.baseURI = "http://localhost:8080/";
+
+        Map<String, String> map = new HashMap<>();
+        map.put("email", "aluno@email.com");
+        map.put("senha", "123456");
+
+        Response response = post(map, ContentType.JSON, "auth");
+
+        assertEquals(200, response.statusCode());
+
+        token = response.jsonPath().get("token");
+        System.out.println(token);
+    }
+
+    @Test
+    public void validarConsultaCategorias(){
+        Map<String, String> header = new HashMap<>();
+        header.put("Authorization", "Bearer " + token);
+
+        Response response = get(header, "categorias");
+        assertEquals(200, response.statusCode());
+
+        System.out.println(response.jsonPath().get().toString());
+    }
+
+    public static Response get(Map<String, String> header, String endpoint) {
+        return RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(header)
+                .when()
+                .get(endpoint)
+                .thenReturn();
+    }
+
+    public static Response post(Object json, ContentType contentType, String endpoint){
+        return RestAssured.given()
+                .relaxedHTTPSValidation()
+                .contentType(contentType)
+                .body(json)
+                .when()
+                .post(endpoint)
+                .thenReturn();
+    }
+}
